cd5265bdd406156f1a202b917747bf7a1dd540e9
Ubuntu <ubuntu@ip-172-31-93-114.ec2.internal>
2019-07-29 14:07:17 +0000

    Correction for DB Connection Error

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
 
 mongo = PyMongo(app)
 
